7016c0754ef91c100b4143088ebe07b0225b4a18
jeena park <jeenapark15@gmail.com>
2022-06-06 17:58:07 -0400

    commit tree 0bd7a429a81073c74b43916ce6a80d5533d5364f

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer") 
+    puts "Hello, #{name}!"
+    # if name.class == String
+    #     puts "Hello, #{name}!"
+    # else name.class != String
+    #     puts "Hello, programmer!"
+    # end
+end
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+def halve(num)
+    if num.class != Integer
+        return nil
+    end
+
+    return num/2
+end
